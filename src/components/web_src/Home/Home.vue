9ed990d87807386b26383ff1916cc9335fe9c21c
AdexeMonzon <adexems.projects@gmail.com>
2026-02-22 13:50:25 +0000

    feat-in-progress/ implement auth check on homepage to distinguish between logged users and guests

--- src/components/web_src/Home/Home.vue
+++ src/components/web_src/Home/Home.vue
@@ -1,11 +1,3 @@
-<script setup>
-import HomeNavComponent from '../Home/home-nav-component.vue';
-import HeroComponent from '../Home/hero-component.vue';
-import ServiceComponent from '../Home/services-components/service-component.vue';
-import SliderComparator from '../UI/slider-comparator.vue';
-import cochelimpio from "@/assets/cochelimpio.jpeg";
-import cochesucio from "@/assets/cochesucio.jpeg";
-</script>
 
 <template>
   <header>
@@ -15,6 +7,64 @@ import cochesucio from "@/assets/cochesucio.jpeg";
     <slider-comparator :before="cochesucio" :after="cochelimpio" />
   </header>
 </template>
+
+  <script>
+  import HomeNavComponent from '../Home/home-nav-component.vue';
+  import HeroComponent from '../Home/hero-component.vue';
+  import ServiceComponent from '../Home/services-components/service-component.vue';
+  import SliderComparator from '../UI/slider-comparator.vue';
+  import cochelimpio from "@/assets/cochelimpio.jpeg";
+  import cochesucio from "@/assets/cochesucio.jpeg";
+
+  export default {
+    components: {
+      HomeNavComponent,
+      HeroComponent,
+      ServiceComponent,
+      SliderComparator
+    },
+    data() {
+      return {
+        cochelimpio,
+        cochesucio,
+        user_verfied: false
+      }
+    },
+
+    methods: {
+      checkUser() {
+        const token = localStorage.getItem('token');
+        if (token) {
+          fetch('http://api/user', {
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Accept': 'application/json'
+            }
+          })
+          .then(response => {
+            if (response.ok) {
+              alert('Usuario verificado');
+              console.log(response++);
+              
+              this.user_verfied = true;
+            } else {
+              localStorage.removeItem('token');
+            }
+          })
+          .catch(error => {
+            console.error('Usuario inválido:', error);
+            localStorage.removeItem('token');
+          });
+        } else {
+          this.user_verfied = false;
+        }
+      }
+    },
+    mounted() {
+      this.checkUser();
+    }
+  }
+  </script>
 
 <style scoped>
 .btn {
